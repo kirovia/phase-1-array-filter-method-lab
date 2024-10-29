f8af15415bddd1dcd55b395af455e4d32bc5ae2e
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-29 00:43:32 -0400

    passes ALL tests WOOHOO!

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const findMatching = (array, string) => array.filter(item => item.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (array, string) => array.filter(item => item.slice(0, string.length) === string);
+
+const matchName = (array, string) => array.filter(item => item.name === string);
